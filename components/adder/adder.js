f6778170bf435e3f0d97e54c7eadb407bec4bdcc
Denise Case <denisecase@gmail.com>
2020-10-02 12:13:37 -0500

    three-part adder

--- components/adder/adder.js
+++ components/adder/adder.js
@@ -1,17 +1,21 @@
-const add = (x, y) => { return x + y }
+const add = (x, y, z) => { return x + y +z }
 
 const adder = new Vue({
   el: '#adder',
   data: {
     guest: 'Emmett',
     firstNumber: 5,
-    secondNumber: 3
+    secondNumber: 3,
+    thirdNumber: 2
   },
   computed: {
     result: function () {
-      const i = parseInt(this.firstNumber)
-      const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+      const i = parseInt(this.firstNumber);
+      const j = parseInt(this.secondNumber);
+      const a = parseInt(this.thirdNumber);
+      // guest is an instance variable of this object - use this.
+      // add is a local function - do NOT use this. :)
+      return `${this.guest}, your sum is ${add(i,j,a)}.`
     }
   }
 })
